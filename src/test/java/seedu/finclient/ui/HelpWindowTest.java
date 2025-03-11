07077eeeb1348572e07c93559af87c1e2c9fa06f
TanJieHaoAmos <156202208+TanJieHaoAmos@users.noreply.github.com>
2025-03-11 21:02:18 +0800

    Add test case for HelpWindow

---
+++ src/test/java/seedu/finclient/ui/HelpWindowTest.java
@@ -0,0 +1,41 @@
+package seedu.finclient.ui;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.util.WaitForAsyncUtils;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+import org.testfx.api.FxRobot;
+
+/**
+ * Test class for HelpWindow.
+ */
+@ExtendWith(ApplicationExtension.class)
+public class HelpWindowTest {
+
+    private HelpWindow helpWindow;
+
+    @Start
+    private void start(Stage stage) {
+        helpWindow = new HelpWindow(stage);
+        helpWindow.show();
+    }
+
+    @Test
+    void testEscapeKeyClosesWindow(FxRobot robot) {
+        // Assert that the HelpWindow is visible initially
+        assertTrue(helpWindow.isShowing());
+
+        // Simulate pressing ESC key
+        robot.press(KeyCode.ESCAPE).release(KeyCode.ESCAPE);
+        WaitForAsyncUtils.waitForFxEvents();
+
+        // Assert that the window is now closed (invisible)
+        assertFalse(helpWindow.isShowing());
+    }
+}
