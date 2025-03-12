de1cf6ab5715d3200fa0c442bff45f04571a34e6
TanJieHaoAmos <156202208+TanJieHaoAmos@users.noreply.github.com>
2025-03-12 20:08:43 +0800

    Fix checkstyle

--- src/test/java/seedu/finclient/ui/HelpWindowTest.java
+++ src/test/java/seedu/finclient/ui/HelpWindowTest.java
@@ -3,7 +3,6 @@ package seedu.finclient.ui;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
