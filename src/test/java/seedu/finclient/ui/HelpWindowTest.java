33045b02a8891facee33fb99f33f2a545696dd0b
TanJieHaoAmos <156202208+TanJieHaoAmos@users.noreply.github.com>
2025-03-15 13:58:10 +0800

    Update HelpWindow testcase

--- src/test/java/seedu/finclient/ui/HelpWindowTest.java
+++ src/test/java/seedu/finclient/ui/HelpWindowTest.java
@@ -3,8 +3,10 @@ package seedu.finclient.ui;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.util.WaitForAsyncUtils;
 
 import javafx.application.Platform;
 import javafx.scene.Scene;
@@ -15,27 +17,35 @@ public class HelpWindowTest extends ApplicationTest {
 
     private HelpWindow helpWindow;
 
+    @BeforeAll
+    public static void initJavaFX() {
+        Platform.startup(() -> {});
+    }
+
     @Override
     public void start(Stage stage) {
-        helpWindow = new HelpWindow(stage);
+        Platform.runLater(() -> helpWindow = new HelpWindow(stage));
+        WaitForAsyncUtils.waitForFxEvents();
     }
 
     @Test
     public void show_helpWindowIsDisplayedAndHandlesEscapeKey() {
-
-        Platform.runLater(() -> {
+        interact(() -> {
             assertFalse(helpWindow.isShowing());
             helpWindow.show();
             assertTrue(helpWindow.isShowing());
         });
 
-        sleep(500);
-        Scene scene = helpWindow.getRoot().getScene();
-        Platform.runLater(() -> scene.getOnKeyPressed().handle(new javafx.scene.input.KeyEvent(
-                javafx.scene.input.KeyEvent.KEY_PRESSED, "", "", KeyCode.ESCAPE, false, false, false, false)));
+        WaitForAsyncUtils.waitForFxEvents();
 
-        sleep(500);
+        interact(() -> {
+            Scene scene = helpWindow.getRoot().getScene();
+            scene.getOnKeyPressed().handle(new javafx.scene.input.KeyEvent(
+                    javafx.scene.input.KeyEvent.KEY_PRESSED, "", "", KeyCode.ESCAPE, false, false, false, false));
+        });
 
-        assertFalse(helpWindow.isShowing());
+        WaitForAsyncUtils.waitForFxEvents();
+
+        interact(() -> assertFalse(helpWindow.isShowing()));
     }
 }
