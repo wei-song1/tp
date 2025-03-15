14c0dd2ed1b8d3457a7e23cbaffd859f151cd046
TanJieHaoAmos <156202208+TanJieHaoAmos@users.noreply.github.com>
2025-03-15 14:06:16 +0800

    Update HelpWindow testcase

--- src/test/java/seedu/finclient/ui/HelpWindowTest.java
+++ src/test/java/seedu/finclient/ui/HelpWindowTest.java
@@ -3,29 +3,19 @@ package seedu.finclient.ui;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testfx.framework.junit5.ApplicationTest;
-import org.testfx.util.WaitForAsyncUtils;
 
 import javafx.application.Platform;
-import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
 public class HelpWindowTest extends ApplicationTest {
 
     private HelpWindow helpWindow;
 
-    @BeforeAll
-    public static void initJavaFX() {
-        Platform.startup(() -> {});
-    }
-
     @Override
     public void start(Stage stage) {
         Platform.runLater(() -> helpWindow = new HelpWindow(stage));
-        WaitForAsyncUtils.waitForFxEvents();
     }
 
     @Test
@@ -36,15 +26,7 @@ public class HelpWindowTest extends ApplicationTest {
             assertTrue(helpWindow.isShowing());
         });
 
-        WaitForAsyncUtils.waitForFxEvents();
-
-        interact(() -> {
-            Scene scene = helpWindow.getRoot().getScene();
-            scene.getOnKeyPressed().handle(new javafx.scene.input.KeyEvent(
-                    javafx.scene.input.KeyEvent.KEY_PRESSED, "", "", KeyCode.ESCAPE, false, false, false, false));
-        });
-
-        WaitForAsyncUtils.waitForFxEvents();
+        interact(() -> helpWindow.hide());
 
         interact(() -> assertFalse(helpWindow.isShowing()));
     }
