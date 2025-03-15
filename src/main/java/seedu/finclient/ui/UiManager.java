e67ecf74cd3c57a9fd6f4976fa8152bf6c644ec4
TanJieHaoAmos <156202208+TanJieHaoAmos@users.noreply.github.com>
2025-03-15 18:48:07 +0800

    Fix checkstyle issue

--- src/main/java/seedu/finclient/ui/UiManager.java
+++ src/main/java/seedu/finclient/ui/UiManager.java
@@ -41,7 +41,7 @@ public class UiManager implements Ui {
 
         try {
             mainWindow = new MainWindow(primaryStage, logic);
-            mainWindow.show();//This should be called before creating other UI parts
+            mainWindow.show(); //This should be called before creating other UI parts
             mainWindow.fillInnerParts();
 
         } catch (Throwable e) {
