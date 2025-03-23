2a95179b6c1366f2be8e02aaa44c264642211524
TanJieHaoAmos <156202208+TanJieHaoAmos@users.noreply.github.com>
2025-03-23 16:59:36 +0800

    Update HelpWindowTest

--- src/test/java/seedu/finclient/ui/HelpWindowTest.java
+++ src/test/java/seedu/finclient/ui/HelpWindowTest.java
@@ -1,8 +1,9 @@
 package seedu.finclient.ui;
 
-//import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,8 +11,13 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import org.testfx.api.FxRobot;
 import org.testfx.api.FxToolkit;
 
-//import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCode;
 
+/**
+ * Tests HelpWindow function
+ * Runs and passes in Windows and MacOS environment
+ * Reuses code from AB4
+ */
 
 public class HelpWindowTest extends StageExtension {
 
@@ -20,36 +26,44 @@ public class HelpWindowTest extends StageExtension {
     private HelpWindow helpWindow;
     private FxRobot robot = new FxRobot();
 
+
     @BeforeEach
     public void setUp() throws Exception {
+        assumeFalse(System.getProperty("os.name").toLowerCase().contains("ubuntu"));
+        assumeFalse(System.getProperty("os.name").toLowerCase().contains("linux"));
         robot.interact(() -> helpWindow = new HelpWindow());
         FxToolkit.registerStage(helpWindow::getRoot);
     }
 
     @Test
     public void testHelpLabel() {
+        assumeFalse(System.getProperty("os.name").toLowerCase().contains("ubuntu"));
+        assumeFalse(System.getProperty("os.name").toLowerCase().contains("linux"));
         assertNotNull(helpWindow.getHelpLabel());
     }
-    /*
+
     @Test
     public void isShowing_helpWindowIsShowing_returnsTrue() {
+        assumeFalse(System.getProperty("os.name").toLowerCase().contains("ubuntu"));
+        assumeFalse(System.getProperty("os.name").toLowerCase().contains("linux"));
         robot.interact(helpWindow::show);
         assertTrue(helpWindow.isShowing());
     }
 
     @Test
     public void isShowing_helpWindowIsHiding_returnsFalse() {
+        assumeFalse(System.getProperty("os.name").toLowerCase().contains("ubuntu"));
+        assumeFalse(System.getProperty("os.name").toLowerCase().contains("linux"));
         assertFalse(helpWindow.isShowing());
     }
 
     @Test
     public void handleHelp() {
+        assumeFalse(System.getProperty("os.name").toLowerCase().contains("ubuntu"));
+        assumeFalse(System.getProperty("os.name").toLowerCase().contains("linux"));
         robot.interact(helpWindow::show);
         robot.press(KeyCode.valueOf("ESCAPE"));
         robot.release(KeyCode.valueOf("ESCAPE"));
         assertFalse(helpWindow.isShowing());
     }
-    */
-
-
 }
