7e5a114e6fac6ea8cd0d764470f446bde1444b00
ki1r0 <D1ckyTwist3r@users.noreply.github.com>
2025-04-07 18:47:56 +0800

    Update address constraints: restrict to alphanumeric and common symbols with clearer message

--- src/main/java/seedu/finclient/model/person/Address.java
+++ src/main/java/seedu/finclient/model/person/Address.java
@@ -9,13 +9,15 @@ import static seedu.finclient.commons.util.AppUtil.checkArgument;
  */
 public class Address {
 
-    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS =
+            "Addresses should contain only alphanumeric characters and "
+                    + "common symbols such as , . / # -, and must not be blank.";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[^\\s].*";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ,.\\-/#]*";
 
     public final String value;
 
